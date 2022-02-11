2171f0aebea041b715244e36ca0d6cf1cb32dedf
Fanxiao Liu <fliu@bayviewglen.ca>
2022-02-11 13:33:59 -0500

    Comments

--- javascript/parserRoster.js
+++ javascript/parserRoster.js
@@ -2,10 +2,12 @@ function parse(pn) {
     let data1;
     let x =3;
     let data2 = [];
+    // Gets the txt file to parse
     fetch('/javascript/data.txt')
     .then(response => response.text())
     .then(data => {
         // Do something with your data
+        // Parses the .txt file line by line and stores each line in an array
         data1 = data.split(/\r\n|\n/);
         for(var line = 0; line < line.length-1; line++){
             console.log(line + " --> "+ line[line]);
@@ -14,6 +16,7 @@ function parse(pn) {
 
         let j = 0;
 
+        // Transfers the data we want from data1 to data2 based on pn
         for (let i = pn; i < data1.length; i++) {
             console.log(i);
             data2[j] = data1[i];
@@ -22,12 +25,15 @@ function parse(pn) {
 
         console.log(data2);
         
-        
+        // Alternative way to do it using queries
         // location.href = "/players.html?name="+data1[1];
+
+        // Stores the information in the sessionstorage so it can be used across multiple pages
         sessionStorage.setItem("data2", JSON.stringify(data2));
 
         var imgurl;
 
+        // imgurl will become useful later but it is a symbol to identify images to be put in the page
         if (pn == 0) {
             imgurl = 0;
         } else {
@@ -40,15 +46,10 @@ function parse(pn) {
     });
 }
 
-function parse1() {
-    let results = function1();
-    console.log(results);
-    let temp = results[1];
+// Query Method, not used here
 
-    document.getElementById("pname").innerHTML = results[1];
-    location.href = "/players.html";
-}
 
+/*
 function get(name){
     if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
        return decodeURIComponent(name[1]);
@@ -58,3 +59,4 @@ function getqueryparams() {
     let name = get("name");
     document.getElementById("pname").innerHTML = name;
 }
+*/
--- javascript/player.js
+++ javascript/player.js
@@ -1,6 +1,9 @@
 function loadInfo() {
+    // Sets data to be a value we stored in parserRoster and imgurl as well
     let data = JSON.parse(sessionStorage.getItem("data2"));
     console.log(data);
+
+    //Changes the html elements on the players page to be the value we stored
     document.getElementById("pname").innerHTML = data[0];
     document.getElementById("page").innerHTML = "Age: " + data[1];
     document.getElementById("pheight").innerHTML = "Height: " + data[2];
@@ -11,8 +14,8 @@ function loadInfo() {
 
     let path = "/imgs/player" + imgurl + ".png";
 
-    var myImg = document.getElementById("playerimg"); // creates a new img element
+    var myImg = document.getElementById("playerimg"); // Stores the element in a variable
 
 
-    myImg.setAttribute('src', path); // adds a src attribute (with the value myPath) to myImg
+    myImg.setAttribute('src', path); // adds a src attribute (with the value path) to myImg
 }
